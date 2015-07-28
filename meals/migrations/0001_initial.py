a0767da0241da3b6afd415abfd660925114137ff
Joost Rijneveld <joost@joostrijneveld.nl>
2015-07-29 00:42:28 +0200

    Redesign models

--- meals/migrations/0001_initial.py
+++ meals/migrations/0001_initial.py
@@ -11,36 +11,57 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
+            name='Bystander',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', auto_created=True, primary_key=True, serialize=False)),
+                ('name', models.CharField(max_length=200)),
+            ],
+        ),
+        migrations.CreateModel(
             name='Meal',
             fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('id', models.AutoField(verbose_name='ID', auto_created=True, primary_key=True, serialize=False)),
                 ('price', models.IntegerField(default=0)),
                 ('date', models.DateTimeField(auto_now=True)),
+                ('completed', models.BooleanField(default=False)),
+                ('description', models.CharField(max_length=200, blank=True)),
+                ('bystanders', models.ManyToManyField(blank=True, to='meals.Bystander')),
             ],
         ),
         migrations.CreateModel(
             name='Participant',
             fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('id', models.AutoField(verbose_name='ID', auto_created=True, primary_key=True, serialize=False)),
+                ('wbw_id', models.IntegerField(unique=True)),
                 ('name', models.CharField(max_length=200)),
-                ('meal', models.ForeignKey(to='meals.Meal')),
             ],
         ),
         migrations.CreateModel(
-            name='WBW_list',
+            name='Wbw_list',
             fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
-                ('list_id', models.IntegerField()),
+                ('id', models.AutoField(verbose_name='ID', auto_created=True, primary_key=True, serialize=False)),
+                ('list_id', models.IntegerField(unique=True)),
+                ('name', models.CharField(max_length=200, blank=True)),
             ],
         ),
         migrations.AddField(
             model_name='participant',
             name='wbw_list',
-            field=models.ForeignKey(to='meals.WBW_list'),
+            field=models.ForeignKey(to='meals.Wbw_list', null=True),
+        ),
+        migrations.AddField(
+            model_name='meal',
+            name='participants',
+            field=models.ManyToManyField(blank=True, to='meals.Participant'),
         ),
         migrations.AddField(
             model_name='meal',
             name='payer',
-            field=models.ForeignKey(to='meals.WBW_list'),
+            field=models.ForeignKey(to='meals.Participant', related_name='paymeal', null=True),
+        ),
+        migrations.AddField(
+            model_name='meal',
+            name='wbw_list',
+            field=models.ForeignKey(to='meals.Wbw_list', null=True),
         ),
     ]
--- meals/models.py
+++ meals/models.py
@@ -1,18 +1,29 @@
 from django.db import models
 
 
-class WBW_list(models.Model):
-    list_id = models.IntegerField()
+class Wbw_list(models.Model):
+    list_id = models.IntegerField(unique=True)
+    name = models.CharField(max_length=200, blank=True)
+
+
+class Participant(models.Model):
+    wbw_list = models.ForeignKey(Wbw_list, null=True)
+    wbw_id = models.IntegerField(unique=True)
+    name = models.CharField(max_length=200)
+
+
+class Bystander(models.Model):
+    name = models.CharField(max_length=200)
 
 
 class Meal(models.Model):
     price = models.IntegerField(default=0)
-    payer = models.ForeignKey(WBW_list, null=True)
     date = models.DateTimeField(auto_now=True)
     completed = models.BooleanField(default=False)
+    description = models.CharField(max_length=200, blank=True)
 
+    wbw_list = models.ForeignKey(Wbw_list, null=True)
+    participants = models.ManyToManyField(Participant, blank=True)
+    bystanders = models.ManyToManyField(Bystander, blank=True)
+    payer = models.ForeignKey(Participant, null=True, related_name='paymeal')
 
-class Participant(models.Model):
-    wbw_list = models.ForeignKey(WBW_list)
-    meal = models.ForeignKey(Meal)
-    name = models.CharField(max_length=200)
