adb18cf13310d4ae88cc99fec1088eb8acaba215
Joost Rijneveld <joost@joostrijneveld.nl>
2015-07-29 00:44:58 +0200

    Register models to admin

--- meals/admin.py
+++ meals/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from meals.models import Meal, Participant, Bystander, Wbw_list
 
-# Register your models here.
+admin.site.register(Meal)
+admin.site.register(Participant)
+admin.site.register(Bystander)
+admin.site.register(Wbw_list)
